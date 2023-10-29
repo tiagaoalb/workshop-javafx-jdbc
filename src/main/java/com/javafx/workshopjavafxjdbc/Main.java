518c0f36087d60f1bea80872f102da55e0745ca2
Tiago <tiagaoalb@gmail.com>
2023-10-29 19:03:33 -0300

    chore: add MainView design

--- src/main/java/com/javafx/workshopjavafxjdbc/Main.java
+++ src/main/java/com/javafx/workshopjavafxjdbc/Main.java
@@ -3,6 +3,7 @@ package com.javafx.workshopjavafxjdbc;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,7 +12,12 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("MainView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        ScrollPane scrollPane = fxmlLoader.load();
+
+        scrollPane.setFitToHeight(true);
+        scrollPane.setFitToWidth(true);
+
+        Scene scene = new Scene(scrollPane);
         stage.setTitle("Sample JavaFX application");
         stage.setScene(scene);
         stage.show();
